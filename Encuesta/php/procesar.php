054e3f912cc23c8e1200827f07f4f8b72846d64e
Helheim969 <56802766+Helheim969@users.noreply.github.com>
2021-10-06 15:18:54 -0600

    Add files via upload

---
+++ Encuesta/php/procesar.php
@@ -0,0 +1,15 @@
+<?php
+$html="";
+$VAR1="<img src='/Encuesta/img/";
+$VAR2=".png' alt='' style='width:100px;height:100px;'>";
+
+foreach($_POST as $item=>$item2){
+
+    $html.="{$VAR1}{$item}{$VAR2}";
+}
+
+echo '<h1 style="text-align:center;">Usted ha seleccionado:</h1> <br>';
+   echo '<div style="text-align:center;">'.$html.'</div>';
+
+
+?>
